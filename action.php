9d65df6f4b6543103df4b12b9abfdf937b11901d
wertui007 <85740152+wertui007@users.noreply.github.com>
2021-06-21 23:24:34 +0200

    Add files via upload

---
+++ action.php
@@ -0,0 +1,3 @@
+Hlavni Teplous <?php echo htmlspecialchars($_POST['name']); ?> <br />
+Je ti <?php echo (int)$_POST['age']; ?> let, ale vypadas na 50.
+
---
+++ zkouska.php
@@ -0,0 +1,5 @@
+<form action="action.php" method="post">
+    <p>Tvoje jmeno: <input type="text" name="name" /></p>
+    <p>Your age: <input type="text" name="age" /></p>
+    <p><input type="submit" /></p>
+</form>
